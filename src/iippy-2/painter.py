178baa208a7993226501bef49f085f50732c0946
nicetag <irislee0923@gmail.com>
2015-04-11 12:19:31 +0800

    circles of each color are in the list but squares and triangles are not. "canvas.draw_polygon(draw_pos[0], 1 , "White", draw_pos[2])"->"undefined: TypeError: undefined is not a function"

--- src/iippy-2/painter.py
+++ src/iippy-2/painter.py
@@ -11,14 +11,15 @@ paint_shape = "circle"
 paint_color = "Lightgrey"
 pos = [WIDTH / 2, HEIGHT / 2]
 shape_radius = 25
+shape_list = []
 
 #define event handler for mouse click, paint
 
-
 #mouse click 
 def click(position):
-    global pos
+    global pos, shape_list
     pos = list(position)
+    shape_list.append([pos,paint_shape,paint_color])
     
 #colors and shapes
 #colors
@@ -53,6 +54,7 @@ def draw(canvas):
     x = pos[0]
     y = pos[1]
     r = shape_radius
+    
     if paint_shape == "triangle":
        canvas.draw_polygon([(x,y+r),(x-r,y-r/2),(x+r,y-r/2)], 1, "White", paint_color)
     elif paint_shape == "square":
@@ -60,10 +62,13 @@ def draw(canvas):
     elif paint_shape == "circle":
        canvas.draw_circle(draw_pos, shape_radius ,1, "White", paint_color)
 
+# record in list
+    for draw_pos in shape_list:
+        if draw_pos[1] =="circle":
+            canvas.draw_circle(draw_pos[0], shape_radius,1,"White", draw_pos[2])
+        else:
+            canvas.draw_polygon(draw_pos[0], 1 , "White", draw_pos[2])
 
-
-
-     
   
 # Create frames and assign callbacks to event handlers
 
