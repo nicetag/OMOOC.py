8674a9d53a6dd1d459c8c775bad205b3f70c26e5
nicetag <irislee0923@gmail.com>
2015-04-26 21:31:37 +0800

    draw(self,canvas): after"for w in self.dots",  "canvas.draw_circle" function should indent from left, or the gradations will not come into force

--- src/iippy-1/painter_watercolor.py
+++ src/iippy-1/painter_watercolor.py
@@ -1,6 +1,5 @@
 
-# Colors
-# Fading Dots
+# Water Color
 
 
 # This program draws randomly colored fading dots that are
@@ -41,12 +40,34 @@ class RGBcolor:
         self.green += 1
         self.blue += 1
         
-class WDot:
+class Dot:
     def __init__(self, pos, color, radius, lifespan):
-        '''lifespan as color steps:
-        '''
         self.pos = pos
         self.color = color
+        self.radius = radius
+        # Without lifespan, the number of dots would increase
+        #   indefinitely, causing the program to slow down.
+        self.life = lifespan
+        
+    def draw(self, canvas):
+        color_string = self.color.make_html()
+        canvas.draw_circle(self.pos, self.radius, 1, color_string, color_string)
+        
+    # Updates the life and causes the color to fade to white.
+    def update(self):
+        self.life -= 1
+        self.color.brighten()
+        self.radius += 0.1
+        #print self.color
+    def get_life(self):
+        return self.life     
+        
+class WDot:
+    def __init__(self, pos, color, radius, lifespan):
+        '''lifespan as color steps... 
+        '''
+        self.pos = pos
+        self.color = color 
         self.radius = radius
         # Without lifespan, the number of dots would increase
         #	indefinitely, causing the program to slow down.
@@ -57,9 +78,9 @@ class WDot:
     def draw(self, canvas):
         for w in self.dots:
             print w
-        #color_string = self.color.make_html()
-        #  [(187, 216), 26.6, 1, 'rgb(338, 374, 415)', 'rgb(338, 374, 415)']
-        canvas.draw_circle(w[0]
+            #color_string = self.color.make_html()
+            #  [(187, 216), 26.6, 1, 'rgb(338, 374, 415)', 'rgb(338, 374, 415)']
+            canvas.draw_circle(w[0]
                           , w[1]
                           , 1
                           , w[3]
@@ -70,13 +91,12 @@ class WDot:
     def _gen_dots(self):
         for d in range(self.life):
             self.color.brighten()
-            color_string = self.color.make_html()
+            color_string = self.color. make_html()
             self.dots.append([self.pos
-                    , (self.radius + (0.43 * d))
+                    , (self.radius +  (0.42 * d))
                     , 1
                     , color_string, color_string
            ])
-        
         #d.draw(canvas) 
         self.dots.reverse()
         #for i in self.dots:
@@ -111,19 +131,20 @@ def wdraw(canvas):
     for d in list(WDOTS):
         #print d.dots
         d.draw(canvas)
-    return None         
+    return None          
            
      
 def click(pos):
-    new_color = RGBcolor(random.randrange(0,256), random.randrange(0,256), random.randrange(0,256))
+    new_color = RGBcolor(random.randrange(0,256), random.randrange(0,256), 
+         random.randrange(0,256))
     print new_color
     print "HTML color is " + new_color.make_html()
     print
-    #dots.append(WDot(pos, new_color, 20, 256))
-    WDOTS.append (WDot(pos, new_color, 25, 43))
+    # dots.append(WDot(pos, new_color, 20, 256))
+    WDOTS.append (WDot(pos, new_color, 7, 42))
     
 # Frame
-
+ 
 frame = simplegui.create_frame("Water Color", canvas_width, canvas_height) 
 
 # Register Event Handlers
